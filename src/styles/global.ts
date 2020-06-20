2098ca367c432cb3dcebcb344b464f88d2f1ec3c
Mateus Andrade <mat.andrade@live.com>
2020-06-20 13:21:57 -0300

    setting up TS project

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  /* body, input, button {
+    font: 16px 'Montserrat', sans-serif;
+  } */
+  #root {
+    margin: 0 auto;
+  }
+  /* h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  } */
+  /* button {
+    cursor: pointer;
+  } */
+`;
