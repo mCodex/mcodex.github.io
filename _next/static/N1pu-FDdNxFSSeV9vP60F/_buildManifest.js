50a5473b833ffe6a4fc3e7b8e70d872b0cecd956
mCodex <mCodex@users.noreply.github.com>
2020-11-01 15:26:37 +0000

    deploy: 0d9dca05ac4d5d4c82e87b695c76c77719a282ca

--- _next/static/N1pu-FDdNxFSSeV9vP60F/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1bfc9850.bc437a9f9bb7839e7dcc.js","static\u002Fchunks\u002Fpages\u002Findex-e424dff1c86439d03d23.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5efcade778114a255845.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/uT5v-a3nJ5UhWMr92JJPP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1bfc9850.bc437a9f9bb7839e7dcc.js","static\u002Fchunks\u002Fpages\u002Findex-bf0f2a4ed329ecb45ee6.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5efcade778114a255845.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
