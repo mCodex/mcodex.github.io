38ecbceff0b013d80444b57314e235d0f7e6d75f
Mateus Andrade <mat.andrade@live.com>
2020-11-01 12:39:47 -0300

    Fixed image on next-seo

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -5,10 +5,24 @@ export default {
   canonical: 'https://mcodex.dev',
   images: [
     {
-      url: 'https://www.mcodex.dev/assets/avatar.jpeg',
+      url: 'https://mcodex.dev/assets/img/avatar.jpeg',
       width: 800,
       height: 600,
       alt: 'Mateus Andrade',
     },
   ],
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://mcodex.dev',
+    site_name: 'mCodex - Mateus Andrade',
+    images: [
+      {
+        url: 'https://mcodex.dev/assets/img/avatar.jpeg',
+        width: 800,
+        height: 600,
+        alt: 'Mateus Andrade',
+      },
+    ],
+  },
 };
