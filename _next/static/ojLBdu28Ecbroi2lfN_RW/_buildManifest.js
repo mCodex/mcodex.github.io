2fe80d07221d930af5b842a77596093b35aa4b6e
mCodex <mCodex@users.noreply.github.com>
2022-12-30 16:21:39 +0000

    deploy: b62d65062cbcc0eea34cc06e2c6483c841f83e68

--- _next/static/ojLBdu28Ecbroi2lfN_RW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-2d36a1ae069c6368.js","static/chunks/pages/index-21aa485ad0cbece8.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
